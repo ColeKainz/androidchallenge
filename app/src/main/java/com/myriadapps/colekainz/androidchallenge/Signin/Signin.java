4da869ab621e21a4e7be1308daa5076ceccc2d52
colekainz <sir.ninjabot@gmail.com>
2018-09-13 10:44:09 -0500

    Initial Commit

---
+++ app/src/main/java/com/myriadapps/colekainz/androidchallenge/Signin/Signin.java
@@ -0,0 +1,15 @@
+package com.myriadapps.colekainz.androidchallenge.Signin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myriadapps.colekainz.androidchallenge.R;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity);
+    }
+}
