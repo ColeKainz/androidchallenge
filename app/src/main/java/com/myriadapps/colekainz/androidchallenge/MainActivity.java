453426fa501943dc35c8d66ef720628d0f3ee87a
colekainz <sir.ninjabot@gmail.com>
2018-09-13 13:24:06 -0500

    Edited Main Activity to start signin, signup, and kingdom activities depending on account information.

--- app/src/main/java/com/myriadapps/colekainz/androidchallenge/MainActivity.java
+++ app/src/main/java/com/myriadapps/colekainz/androidchallenge/MainActivity.java
@@ -1,13 +1,72 @@
 package com.myriadapps.colekainz.androidchallenge;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.myriadapps.colekainz.androidchallenge.InformationBoard.Kingdoms;
+import com.myriadapps.colekainz.androidchallenge.Signin.Signin;
+import com.myriadapps.colekainz.androidchallenge.Signin.Signup;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindString;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindString(R.string.load_user_failed) String loadUserFailedMSG;
+
+    //Signin info.
+    @BindString(R.string.saved_accounts) String savedAccountsString;
+    @BindString(R.string.default_account) String defaultAccountString;
+    @BindString(R.string.sigin_name) String signinName;
+    @BindString(R.string.sigin_email) String signinEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String savedAccounts = prefs.getString(savedAccountsString, null);
+        String defaultAccount = prefs.getString(defaultAccountString, null);
+
+        //Check if there are saved accounts.
+        //If not, default to signup.
+        if(savedAccounts != null && !savedAccounts.isEmpty()) {
+            if(defaultAccount != null && !defaultAccount.isEmpty()) {
+                //Try to load default user.
+                try {
+                    JSONObject defaultSignin = new JSONObject(defaultAccount);
+
+                    String name = defaultSignin.getString("name");
+                    String email = defaultSignin.getString("email");
+
+                    Intent kingdoms = new Intent(this, Kingdoms.class);
+                    kingdoms.putExtra(signinName, name);
+                    kingdoms.putExtra(signinEmail, email);
+                    startActivity(kingdoms);
+                } catch(JSONException e) {
+                    Toast errMsg = Toast.makeText(this, loadUserFailedMSG,
+                            Toast.LENGTH_LONG);
+                    errMsg.show();
+
+                    //Cannot load user, so default to sign in screen.
+                    Intent signin = new Intent(this, Signin.class);
+                    startActivity(signin);
+                }
+            } else {
+                //No default account, load signin.
+                Intent signin = new Intent(this, Signin.class);
+                startActivity(signin);
+            }
+        } else {
+            Intent signup = new Intent(this, Signup.class);
+            startActivity(signup);
+        }
     }
 }
--- app/src/main/java/com/myriadapps/colekainz/androidchallenge/Signin/Signup.java
+++ app/src/main/java/com/myriadapps/colekainz/androidchallenge/Signin/Signup.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.myriadapps.colekainz.androidchallenge.R;
 
-public class Signin extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
     }
 }
